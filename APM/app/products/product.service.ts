7a7b47e8426ba0bf7cb099c0285570d1f8057045
Evandro Oliveira <oliveiras.evandro@gmail.com>
2016-12-02 14:19:04 -0200

    Getting Started with Angular 2

---
+++ APM/app/products/product.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {IProduct} from "./product";
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs/Rx";
+/**
+ * Created by eso on 02/12/16.
+ */
+
+@Injectable()
+export class ProductService {
+
+    private _productUrl = 'api/products/products.json';
+
+    constructor(private _http: Http) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get(this._productUrl)
+            .map((response: Response) => <IProduct[]> response.json())
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+}
---
+++ APM/app/products/product.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by eso on 02/12/16.
+ */
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
